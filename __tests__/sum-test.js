30b2f81840dd0fec53ccf9dc1ca20f8d14f56c56
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-19 16:42:09 +0300

    Initial commit

---
+++ __tests__/sum-test.js
@@ -0,0 +1,8 @@
+jest.dontMock('../src/sum');
+
+describe('sum', function() {
+ it('adds 1 + 2 to equal 3', function() {
+   var sum = require('../src/sum');
+   expect(sum(1, 2)).toBe(3);
+ });
+});
