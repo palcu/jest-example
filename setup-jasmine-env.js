5e1d9dbd85ec77f1d327c6aa0360f17ef0bb9cf4
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-08-05 17:27:31 +0300

    Add jasmine-reporter

---
+++ setup-jasmine-env.js
@@ -0,0 +1,13 @@
+/**
+ * Add custom settings to Jasmine.
+ */
+
+/*globals jasmine*/
+
+jasmine.VERBOSE = true;
+
+require('jasmine-reporters');
+var reporter = new jasmine.JUnitXmlReporter({
+  savePath: "output/"
+});
+jasmine.getEnv().addReporter(reporter);
