46810bbd8bfc200990e6db41f558f191191c6002
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-08-05 16:13:35 +0300

    Add debugger statement

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -1,8 +1,9 @@
 jest.dontMock('../src/sum');
 
 describe('sum', function() {
- it('adds 1 + 2 to equal 3', function() {
-   var sum = require('../src/sum');
-   expect(sum(1, 2)).toBe(3);
- });
+  it('adds 1 + 2 to equal 3', function() {
+    debugger;
+    var sum = require('../src/sum');
+    expect(sum(1, 2)).toBe(3);
+  });
 });
