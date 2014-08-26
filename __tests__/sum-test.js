ea0cf03272b3b4f5729fc3c4d20afe84354910b3
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-08-26 16:33:06 +0300

    Working version

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -1,8 +1,9 @@
 jest.dontMock('../src/sum');
+jest.dontMock('fs');
+jest.dontMock('path');
 
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
-    debugger;
     var sum = require('../src/sum');
     expect(sum(1, 2)).toBe(3);
   });
